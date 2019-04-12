6af1cbbc0b4f12613be005796f66ac55a5a01507
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-12 22:35:21 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(arr, element) {
+  return [element, ...arr]];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  arr.unshift(element);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, element) {
+  return [...arr, element];
+}
+
+function destructivelyAddElementToEndOfArray(arr, element) {
+  arr.push(element);
+  return arr;
+}
+
+function accessElementInArray(arr, idx) {
+  return arr[idx];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
