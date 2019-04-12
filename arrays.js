ec888465c730bce15e20dbd60a14416406234a61
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-12 22:38:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, element) {
-  return [element, ...arr]];
+  return [element, ...arr];
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, element) {
